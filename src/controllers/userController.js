a65a3681e96df79a6782a7e084d4efc4159bbefc
yonexdu0ra <phamngocquy.7303@gmail.com>
2025-02-07 20:29:27 +0700

    update source code server

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-import { User } from "../model/index.js";
+import { Account, User } from "../model/index.js";
 
 class UserController {
   async myInfo(req, res) {
@@ -17,6 +17,30 @@ class UserController {
     } catch (error) {
       return res
         .status(404)
+        .json({ status: "error", message: error.message || "Lỗi" });
+    }
+  }
+  async changePassword(req, res) {
+    try {
+      const userId = req.userId;
+      const { oldPassword, newPassword, confirmNewPassword } = req.body;
+
+      if(newPassword !== confirmNewPassword) throw new Error("Mật khẩu không khớp nhau.");
+      if(newPassword.length < 6) throw new Error("Mật khẩu mới quá ngắn.");
+      const accountData = await Account.findOne({ where: { userId } });
+      if (!accountData) throw new Error("Cập nhật mật khẩu không thành công.");
+      if(accountData.password !== oldPassword) throw new Error("Mật khẩu cũ chính xác đúng.");
+
+      await Account.update({ password: newPassword }, { where: { userId } });
+
+      return res.status(200).json({
+        status: "success",
+        message: "Cập nhật mật khẩu thành công.",
+      })
+
+    } catch (error) {
+      return res
+        .status(400)
         .json({ status: "error", message: error.message || "Lỗi" });
     }
   }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ import authentication from "../middleware/authentication.js";
 const router = Router();
 
 router.get("/", authentication, userController.myInfo);
+router.post("/change-password", authentication, userController.changePassword);
 
 
 export default router;
